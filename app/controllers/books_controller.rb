39b18d0e5d6469d17d4e52d529c42e2f660d62f9
wanjiku <annritawanjiku77@gmail.com>
2023-03-16 12:56:01 +0300

    added custom rendering in show and index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
     def index
         books = Book.all
-        render json: books
+        render json: books, except: [:created_at, :updated_at], methods: [:summary]
     end
 
     def show
         book = Book.find_by(id: params[:id])
-        render json: book
+        render json: book,  except: [:created_at, :updated_at], methods: [:summary]
     end
     
 end
