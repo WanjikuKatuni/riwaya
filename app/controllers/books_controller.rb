f2be64c2d9f3340b760da4fb413c9458d9017775
wanjiku <annritawanjiku77@gmail.com>
2023-03-21 10:36:09 +0300

    added rescue exception handling

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,9 @@ class BooksController < ApplicationController
     # disable wrap params feature
     wrap_parameters format: []
 
+    # render not found response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     # show all books
 
     def index
@@ -13,13 +16,25 @@ class BooksController < ApplicationController
 
     # show book by ID
 
+    # def show
+    #     book = find_book
+    #     if book
+    #         render json: book,  except: [:id, :created_at, :updated_at], methods: [:title_author], status: :ok
+    #     else
+    #         render_not_found_response
+    #     end
+    # end
+
+    # def show
+    #     book = find_book
+    #     render json: book
+    # rescue ActiveRecord::RecordNotFound
+    #     render_not_found_response
+    # end
+
     def show
         book = find_book
-        if book
-            render json: book,  except: [:id, :created_at, :updated_at], methods: [:title_author], status: :ok
-        else
-            render_not_found_response
-        end
+        render json: book
     end
 
     # create new book
@@ -31,33 +46,52 @@ class BooksController < ApplicationController
 
     # edit existing book
 
-    def update 
-        # find
+    # def update 
+    #     # find
+    #     book = find_book
+    #     if book
+    #         # update
+    #         book.update(book_params)
+    #         render json: book, status: :accepted
+    #     else
+    #         render_not_found_response
+    #     end
+    # end
+
+    # def update
+    #     book = find_book
+    #     book.update(book_params)
+    #     render json: book, status: :accepted
+    # rescue ActiveRecord::RecordNotFound
+    #     render_not_found_response
+    # end
+
+    def update
         book = find_book
-        if book
-            # update
-            book.update(book_params)
-            render json: book, status: :accepted
-        else
-            render_not_found_response
-        end
+        book.update(book_params)
+        render json: book
     end
 
     # delete existing book
 
-    def destroy
-        # find book
-        book = find_book
-        if book
-            # delete
-            book.destroy
-            render json: {}
-            # head :no_content
-        else
-            render_not_found_response
-        end
-    end
+    # def destroy
+    #     # find book
+    #     book = find_book
+    #     if book
+    #         # delete
+    #         book.destroy
+    #         render json: {}
+    #         # head :no_content
+    #     else
+    #         render_not_found_response
+    #     end
+    # end
 
+    def destroy
+        book = find_book
+        book.destroy
+        render json: {}
+    end
 
 
 
@@ -87,9 +121,13 @@ class BooksController < ApplicationController
         render json: {error: "Book not found"}, status: :not_found
     end
 
-    # find book
+    # # find book
+
+    # def find_book
+    #     Book.find_by(id: params[:id])
+    # end
     def find_book
-        Book.find_by(id: params[:id])
+        Book.find(params[:id])
     end
 
 
